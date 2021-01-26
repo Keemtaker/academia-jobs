4ce019480d5dedbab7b1bbb65cc189714731795f
Hakeem <babahakeem@outlook.com>
2021-01-26 21:26:02 +0100

    show page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -19,7 +19,7 @@ class JobsController < ApplicationController
   end
 
   def show
-
+    @job = Job.find(params[:id])
   end
 
   private
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,21 @@
 class Job < ApplicationRecord
   has_rich_text :description
   has_one_attached :logo
+  after_validation :set_slug, only: [:create, :update]
+
+  def to_param
+    "#{id}-#{slug}"
+  end
 
   def country_name
     nation = ISO3166::Country[country]
     nation.translations[I18n.locale.to_s] || nation.name
+  end
+
+  private
+
+  def set_slug
+    self.slug = title.to_s.parameterize
   end
 
 end
