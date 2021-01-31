fd75bf11aa247f1ef05d7aba08d295540180cbca
Hakeem <babahakeem@outlook.com>
2021-01-31 18:20:30 +0100

    pages design

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'jobs#index'
+  root to: 'pages#home'
   mount StripeEvent::Engine, at: '/stripe-webhooks'
   resources :jobs do
     get 'payment', to: 'jobs#payment'
   end
 
   get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
